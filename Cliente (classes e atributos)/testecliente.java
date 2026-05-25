c8cce757a5d9871f3a1525a40f1727232327fb5f
helenamartins252 <mariahelenahellena9@gmail.com>
2026-05-25 18:26:05 -0300

    Delete Cliente (classes e atributos) directory

--- Cliente (classes e atributos)/testecliente.java
+++
@@ -1,6 +0,0 @@
-public class testecliente {
-    public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Maria Santos", "123.456.789-99", 21, "(81)99999-9999");
-        cliente1.visualizarCliente();
-    }
-}
