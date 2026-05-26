eded85b6f5335c837427cd9686ad24114d714396
helenamartins252 <mariahelenahellena9@gmail.com>
2026-05-26 17:57:40 -0300

    Add files via upload

---
+++ testecliente.java
@@ -0,0 +1,6 @@
+public class testecliente {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Maria Santos", "123.456.789-99", 21, "(81)99999-9999");
+        cliente1.visualizarCliente();
+    }
+}
