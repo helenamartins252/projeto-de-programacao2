96e4f2a0f1503dfbfb864634cbff41bddf29cd17
Maria Helena <mariahelenahellena9@gmail.com>
2026-06-30 18:08:19 -0300

    Pessoa

---
+++ pessoa (projeto finalizado)/Pessoa.java
@@ -0,0 +1,33 @@
+public class Pessoa {
+    
+
+    protected String nome;
+    protected String cpf;
+    protected int idade;
+
+    public Pessoa(String nome, String cpf, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Idade: " + idade);
+    }
+}
+    
+
